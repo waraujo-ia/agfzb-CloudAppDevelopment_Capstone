fc0c652a954cb3ec2ec4c81cb5e0e7dd1e5f24d0
Wilfrido Araujo <waraujo@ia.com.mx>
2023-12-21 16:54:24 -0500

    new models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='Name')
+    description = models.CharField(null=False, max_length=30, default='Description')
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " " + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +26,31 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'WAGON')
+    ]
+    dealer_id =  models.IntegerField()
+    name = models.CharField(null=False, max_length=100, default='name')
+    description = models.CharField(max_length=500)
+    type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+    year = models.IntegerField()
+    carMade = models.ManyToManyField(CarMake)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "Description: " + self.type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
